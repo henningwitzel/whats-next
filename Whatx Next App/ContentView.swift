9b15c33a15f6186e92fef9b07374976b6b69587e
Henning Witzel <privat@henning-witzel.de>
2019-11-16 17:38:01 +0100

    Initial Commit
    Created a new project in xcode

---
+++ Whatx Next App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Whatx Next App
+//
+//  Created by Henning Witzel on 16.11.19.
+//  Copyright © 2019 Henning Witzel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Whatx Next AppTests/Whatx_Next_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Whatx_Next_AppTests.swift
+//  Whatx Next AppTests
+//
+//  Created by Henning Witzel on 16.11.19.
+//  Copyright © 2019 Henning Witzel. All rights reserved.
+//
+
+import XCTest
+@testable import Whatx_Next_App
+
+class Whatx_Next_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
